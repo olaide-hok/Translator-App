24125683156db55863e268d55da97770652fd768
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-10 09:17:27 +0100

    Create a field component

---
+++ src/components/field.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Flower from './svg/flower'
+
+interface FieldProps {
+  value: string
+  onChange: Function
+}
+
+function Field({value, onChange}: FieldProps) {
+  return (
+    <div className="field">
+      <Flower className="flower-right" fill="rgb(110, 146, 119)" />
+      <Flower className="flower-left" fill="rgb(249, 148, 59)" />
+      <h1>Translate App</h1>
+      <label>Enter English</label>
+      <input
+        title="input"
+        className="input"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
+  )
+}
+
+export default Field

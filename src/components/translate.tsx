ac7731204b2cbc1ac3c62d1e8eaa0c577955e250
Habeeb Kareem <olaidekareem43@yahoo.com>
2022-12-10 09:16:03 +0100

    Create a translate functions file

---
+++ src/components/translate.tsx
@@ -0,0 +1,75 @@
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
+
+type StringProps = {
+  language: string
+  text: string
+}
+
+function Translate({language, text}: StringProps) {
+  const [translated] = useTranslation(text, language)
+
+  return (
+    <div className="translate">
+      <label className="label">Output</label>
+      <h1 className="title">{translated.replace('&#39;', "'")}</h1>
+    </div>
+  )
+}
+
+const useTranslation = (text: string, language: string) => {
+  const [translated, setTranslated] = useState('')
+
+  useEffect(() => {
+    if (!text) {
+      return
+    }
+
+    const cancelToken = axios.CancelToken.source()
+
+    doTranslation(text, language, cancelToken, setTranslated)
+
+    return () => {
+      try {
+        cancelToken.cancel()
+      } catch (err) {}
+    }
+  }, [text, language])
+
+  return [translated]
+}
+
+const debounce = (fn: any) => {
+  let id: any = null
+
+  return (...args: any[]) => {
+    if (id) {
+      clearTimeout(id)
+    }
+    id = setTimeout(() => {
+      fn(...args)
+      id = null
+    }, 300)
+  }
+}
+
+const doTranslation = debounce(
+  async (input: any, languageCode: any, cancelToken: any, callback: any) => {
+    try {
+      const {data} = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2?key=AIzaSyCf0Xy0OnhxlduyEt3K8zP-sOuu-l_u6uA',
+        {
+          q: input,
+          target: languageCode,
+        },
+        {cancelToken: cancelToken.token}
+      )
+
+      callback(data.data.translations[0].translatedText)
+    } catch (err) {
+      callback('')
+    }
+  }
+)
+
+export default Translate
